8a735d27ad08892011f75708f6901ba25a78c132
satanasdiabolo <philippe@melot.eu>
2012-09-07 14:42:48 +0000

    add default address if application.URL not define in app.ini
    git-svn-id: http://maniaplanet-dedicated-manager.googlecode.com/svn/trunk@120 7f24f777-0cea-ae19-a2bf-98aad5406375

--- www/index.php
+++ www/index.php
@@ -3,5 +3,6 @@ define('DEDICATED_MANAGER_VERSION', '1.5.1');
 
 require_once __DIR__.'/../libraries/autoload.php';
 I18n\Functions::LOAD;
+\ManiaLib\Application\Config::getInstance()->URL = 'http://'.$_SERVER['HTTP_HOST'].':'.$_SERVER['SERVER_PORT'].'/manager/';
 \ManiaLib\Application\Bootstrapper::run();
 ?>
