e67305e91b399b5ef6c56755b578dfb29da6a99b
satanasdiabolo <philippe@nadeo.com>
2014-03-18 10:35:27 +0100

    Change error reporting to avoid deprecated errors

--- www/index.php
+++ www/index.php
@@ -4,5 +4,6 @@ define('DEDICATED_MANAGER_VERSION', '1.8');
 require_once __DIR__.'/../libraries/autoload.php';
 I18n\Functions::LOAD;
 \ManiaLib\Application\Config::getInstance()->URL = 'http://'.$_SERVER['HTTP_HOST'].':'.$_SERVER['SERVER_PORT'].'/manager/';
+\ManiaLib\Application\Bootstrapper::$errorReporting = E_ALL ^ E_DEPRECATED;
 \ManiaLib\Application\Bootstrapper::run();
 ?>
