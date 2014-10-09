05bb20b2d3e9157ddf03b7c57f4c5ea4c517ac7b
philippe <philippe@nadeo.com>
2014-10-09 14:33:27 +0200

    Changed version number

--- www/index.php
+++ www/index.php
@@ -1,9 +1,8 @@
 <?php
-define('DEDICATED_MANAGER_VERSION', '2.1');
+define('DEDICATED_MANAGER_VERSION', '2.1.1');
 
 require_once __DIR__.'/../libraries/autoload.php';
 I18n\Functions::LOAD;
 \ManiaLib\Application\Config::getInstance()->URL = 'http://'.$_SERVER['HTTP_HOST'].':'.$_SERVER['SERVER_PORT'].'/manager/';
 \ManiaLib\Application\Bootstrapper::$errorReporting = E_ALL ^ E_DEPRECATED;
 \ManiaLib\Application\Bootstrapper::run();
-?>
